c47a028591109c2ccea2cf58770b6322900d772f
Calixto Macedo <calixto.macedo@gmail.com>
2023-10-27 09:11:38 -0300

    Iniciando o Curso de Typescript parte 3: mais técnicas e boas práticas

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ npm install -g npm-check-updates // caso apareça algum fix
 * 1.0 Javascript ou Typescript?
 * 1.1 typescript-curso-i - Curso de TypeScript parte 1: evoluindo seu JavaScript: https://cursos.alura.com.br/course/typescript-evoluindo-javascript
 * 1.2 typescript-curso-ii - Curso de TypeScript parte 2: avançando na linguagem: https://cursos.alura.com.br/course/typescript-avancando-linguagem
+* 1.3 typescript-curso-iii - Curso de Typescript parte 3: mais técnicas e boas práticas: https://cursos.alura.com.br/course/typescript-tecnicas-boas-praticas
 
 - 02 angular: Explore o Framework Angular. Aprenda a trabalhar com o Framework criado e mantido pelo Google https://cursos.alura.com.br/formacao-angular-14
 * 1.0 Como começar com Angular: https://www.alura.com.br/artigos/como-comecar-com-angular?_gl=1*1okukl6*_ga*NTYzOTk0ODIzLjE2Nzk0NDMwMjM.*_ga_1EPWSW3PCS*MTY5Nzc5NDk1OS4zOC4xLjE2OTc3OTc5NDUuMC4wLjA.*_fplc*UFVXM05HM3c4bjZFMlI4dGd2cWM2Q3BvWHQ1RkVpY0xSUHl5b3VzJTJCeW9OaTIlMkJRN21LeldiM25WblMlMkZtWGtGZDRITXdtVG83aUNGdFpHZVBVdUlXR3NvcEZWNWh4JTJGRVMzaWhZbWc3Z0NnR0hSdVVhVXNvSHJWb3U0bmRiU0ElM0QlM0Q.
---
+++ typescript/typescript-curso-iii/app/controllers/negociacao-controller.ts
@@ -0,0 +1,45 @@
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+import { MensagemView } from '../views/mensagem-view.js';
+import { NegociacoesView } from '../views/negociacoes-view.js';
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private mensagemView = new MensagemView('#mensagemView');
+
+    constructor() {
+        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
+        this.negociacoesView.update(this.negociacoes);
+    }
+
+    public adiciona(): void {
+        const negociacao = Negociacao.build(this.inputData.value, this.inputQuantidade.value, this.inputValor.value)
+        if(this.ehDiaUltil(negociacao.data)){
+            this.negociacoes.adiciona(negociacao);
+            this.negociacoesView.update(this.negociacoes);
+            this.mensagemView.update('Negociação adicionada com sucesso');
+            this.limparFormulario();
+        } else{
+            this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
+        }
+
+    }
+
+    private ehDiaUltil(data: Date) {
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+    }
+
+    private limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
