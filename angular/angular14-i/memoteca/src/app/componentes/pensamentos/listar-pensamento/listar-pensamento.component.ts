bfdf1b15028159911d05f4710005a44caed0655a
Calixto Macedo <calixto.macedo@gmail.com>
2023-11-10 07:05:51 -0300

    angular14-i memoteca, 04 - Diretivas

--- angular/angular14-i/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular/angular14-i/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
+export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente Pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input',
+      autoria: 'Componente Filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Morbi viverra placerat justo, vel pharetra turpis. In elementis mé pra quem é amistosis quis leo. Diuretics paradis num copo é motivis de denguis. Suco de cevadiss deixa as pessoas mais interessantis. Suco de caniss deixa as pessoas mais interessantis.',
+      autoria: 'Mussum',
+      modelo: 'modelo1'
+    }
+
+  ];
+  constructor(){}
+
+  ngOnInit(): void {}
+
 
 }
