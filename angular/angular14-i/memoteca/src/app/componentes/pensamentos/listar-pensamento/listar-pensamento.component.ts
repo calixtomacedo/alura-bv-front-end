f194e6ac4c78a3334a85b20445b12bdb05ece2e2
Calixto Macedo <calixto.macedo@gmail.com>
2023-11-10 08:00:01 -0300

    angular14-i memoteca, 05 - Comunicação com o backend

--- angular/angular14-i/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ angular/angular14-i/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,25 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
 
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente Pai',
-      modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input',
-      autoria: 'Componente Filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Morbi viverra placerat justo, vel pharetra turpis. In elementis mé pra quem é amistosis quis leo. Diuretics paradis num copo é motivis de denguis. Suco de cevadiss deixa as pessoas mais interessantis. Suco de caniss deixa as pessoas mais interessantis.',
-      autoria: 'Mussum',
-      modelo: 'modelo1'
-    }
+  //listaPensamentos: Pensamento[] = []
+  listaPensamentos: Array<Pensamento> = [];
 
-  ];
   constructor(){}
 
   ngOnInit(): void {}
