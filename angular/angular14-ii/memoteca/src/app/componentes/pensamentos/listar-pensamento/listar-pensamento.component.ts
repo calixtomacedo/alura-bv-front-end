f9333fa13dba57ffd45bbee3af9ea609f2160977
Calixto Macedo <calixto.macedo@gmail.com>
2023-11-13 19:05:30 -0300

    Iniciando o Curso de Angular 14: evoluindo a aplicação

---
+++ angular/angular14-ii/memoteca/src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  //listaPensamentos: Pensamento[] = []
+  listaPensamentos: Array<Pensamento> = [];
+
+  constructor(private service: PensamentoService){}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((pensamentos) => {
+      this.listaPensamentos = pensamentos;
+    });
+  }
+
+
+}
