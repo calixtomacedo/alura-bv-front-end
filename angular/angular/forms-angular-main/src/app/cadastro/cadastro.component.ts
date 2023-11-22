3516bf35383847b26168f62cb8a3c9268f65c395
Calixto Macedo <calixto.macedo@gmail.com>
2023-11-22 08:26:19 -0300

    Finalizando o curso / capitulo 05 - Consumindo dados de API

--- angular/angular/forms-angular-main/src/app/cadastro/cadastro.component.ts
+++ angular/angular/forms-angular-main/src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,19 +10,40 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router, 
+    private consultaCepService: ConsultaCepService
+    ) { }
 
   ngOnInit(): void {
   }
 
-  cadastrar(form: NgForm){
+  public cadastrar(form: NgForm){
     if (form.valid) {
       this.router.navigate(['./sucesso']);
     }else{
       alert('Formulário inválido')
     }
-
-    console.log(form.controls);
   }
-  
+
+  public consultaCEP(ev: any, f: NgForm){
+    const cep = ev.target.value;
+    if(cep !== ""){
+      this.consultaCepService.getConsultaCep(cep).subscribe((response) => {
+        console.log(response);
+        this.populandoEndereco(response, f);
+      });     
+    }
+  }
+
+  private populandoEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+  }
+
 }
