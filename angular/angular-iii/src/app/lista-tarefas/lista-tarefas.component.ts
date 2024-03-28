fcdbf58e846d1ea04cf80a277e96474c0e451f08
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2024-03-28 06:54:31 -0300

    angular-iii: 01.Busca animada com keyframes, offset e cubic-bezier

--- angular/angular-iii/src/app/lista-tarefas/lista-tarefas.component.ts
+++ angular/angular-iii/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,23 +4,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import { checkButtonTrigger, filterTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
 
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger]
+  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger, filterTrigger]
 })
 
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
+  tarefasFiltradas: Tarefa[] = [];
   formAberto: boolean = false;
   categoria: string = '';
   validado: boolean = false;
   indexTarefa = -1;
   id: number = 0;
+  campoBusca: string = '';
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -39,8 +41,9 @@ export class ListaTarefasComponent implements OnInit {
   ngOnInit(): Tarefa[] {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
       this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
-    return this.listaTarefas;
+    return this.tarefasFiltradas;
   }
 
   mostrarOuEsconderFormulario() {
@@ -123,7 +126,7 @@ export class ListaTarefasComponent implements OnInit {
 
   listarAposCheck() {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
   }
 
@@ -145,4 +148,14 @@ export class ListaTarefasComponent implements OnInit {
       return 'form-tarefa';
     }
   }
+
+  filtrarTarefasPorDescricao(descricao: string) {
+    this.campoBusca = descricao.trim().toLowerCase();
+    if(descricao) {
+      this.tarefasFiltradas = this.listaTarefas.filter(tarefa => tarefa.descricao.toLowerCase().includes(this.campoBusca));
+    } else {
+      this.tarefasFiltradas = this.listaTarefas;
+    }
+  }
+
 }
