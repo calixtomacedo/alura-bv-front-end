a079a4135c7a7af977146c373f9dbcbb071c5627
Calixto Rodrigues Macedo <calixto.macedo@gmail.com>
2024-04-19 06:24:34 -0300

    angular-iii: 05. Carregando a lista de forma animada

--- angular/angular-iii/src/app/animations.ts
+++ angular/angular-iii/src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, keyframes, query, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, query, stagger, state, style, transition, trigger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightedState', [
     state('default', style({
@@ -105,3 +105,28 @@ export const highlightedStateTrigger = trigger('highlightedState', [
       ], {optional: true})
     ])
   ])
+
+export const listStateTrigger = trigger('listState', [
+  transition('* => *', [
+    query(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)'
+      }),
+      stagger(200, [
+        animate('500ms ease-out', keyframes([
+          style({
+            opacity: 1,
+            transform: 'translateX(15%)',
+            offset: 0.4
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(0)',
+            offset: 1
+          })
+        ]))
+      ])
+    ], {optional: true})
+  ])
+])
